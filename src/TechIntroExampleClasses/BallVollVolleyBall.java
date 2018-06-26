2ce544cb48420f2ce9ad60c4ab81b72874fe2055
Liliana Gutierrez <lilianagf.750@gmail.com>
2018-06-25 20:11:10 -0500

    Ch1 and Ch2

---
+++ src/TechIntroExampleClasses/BallVollVolleyBall.java
@@ -0,0 +1,17 @@
+/*TASK First TechIntro VolleyBall Example*/ 
+
+//                                      		    //AUTHOR: Towa (LGF-Liliana Gutierrez).
+//                                      		    //CO-AUTHOR: Towa ( ).
+//                                      		    //DATE: February 12th, 2018.
+//                                      		    //PURPOSE: To model a VolleyBall class
+
+package TechIntroExampleClasses;
+
+//======================================================================================================================
+public class BallVollVolleyBall
+{
+    public static final int intDIAMETER = 21;
+}
+
+//======================================================================================================================
+/*END-TASK*/ 
